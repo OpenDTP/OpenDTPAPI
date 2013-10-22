22add260b8992745042f3597862efd3cffbeb518
exadeci <exadeci@vm-1.exadeci.koding.kd.io>
2013-10-22 12:49:38 +0000

    Implementation of Query Structure and Getters/Setters

--- library/ODTPFramwork/Renderer/Query/Abstract.php
+++ library/ODTPFramwork/Renderer/Query/Abstract.php
@@ -5,5 +5,50 @@
 */
 class ODTPFramwork_Renderer_Query_Abstract implements ODTPFramwork_Renderer_Query_Interface
 {
-	
+    private $_input;
+    private $_output;
+    private $_action;
+    private $_params;
+    
+    // Members
+    
+    public function _toString()
+    {
+        
+    }
+    
+    // Setters
+    
+    public function setInput($src)
+    {
+        $this->$_input = $src;
+    }
+
+    public function setOutput($src)
+    {
+        $this->$_output = $src;
+    }
+    
+    public function setAction($action, $params)
+    {
+        $this->$_action = $action;
+        $this->$_params = $params;
+    }
+    
+    // Getters
+    
+    public function getInput()
+    {
+        return $this->$_input;
+    }
+    
+    public function getOutput()
+    {
+        return $this->$_output;
+    }
+    
+    public function getAction()
+    {
+        return $this->$_action;
+    }
 }
--- library/ODTPFramwork/Renderer/Query/Interface.php
+++ library/ODTPFramwork/Renderer/Query/Interface.php
@@ -3,5 +3,9 @@
 /**
  * Interface for renderer query object
  */
-interface ODTPFramwork_Renderer_Query_Interface {
+interface ODTPFramwork_Renderer_Query_Interface
+{
+    public function input($src);
+    public function output($src);
+    public function action($params);
 }
