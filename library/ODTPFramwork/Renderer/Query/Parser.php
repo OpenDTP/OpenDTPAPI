f00a99afb7813603e9655c087da0b195dca8234b
Gaetan Gueraud <exadeci@gmail.com>
2013-10-31 12:55:30 +0100

    Parser edition

--- library/ODTPFramwork/Renderer/Query/Parser.php
+++ library/ODTPFramwork/Renderer/Query/Parser.php
@@ -4,9 +4,10 @@ class ODTPFramwork_Renderer_Query_Parser
 {
 	private $_inputArray = NULL;
 	private $_outputArray = NULL;
-	private $_optionsArray = array(array("render", "output"),
-								   array("update", "in")
+	private $_optionsArray = array("render" => "output",
+								   "update" => "in"
 								  );
+
 /**
 * parses the provided command
 * @return multidimensional array
@@ -14,17 +15,18 @@ class ODTPFramwork_Renderer_Query_Parser
 private function parseString(string $str)
 {
 	private string $_arrayName = "input";
-
+	private string $_key = '';
 	$_inputArray = explode(" ", $str);
-	if (strcasecmp(array_shift(array_values($_inputArray)), "render"))
+	$_key = array_search($_inputArray[0], $_optionsArray)
+	if (!empty($_key))
 	{
-		foreach ($_inputArray as $element)
+		foreach ($_inputArray as $_element)
 		{
-			if ((strcasecmp($element, "output")))
+			if ((strcasecmp($_element, $_key)))
 				$_arrayName = "output";
 
-			if (!(strcasecmp($_arrayName, "render")))
-				$_outputArray[$_arrayName] = $element;	
+			if (!(strcasecmp($_element, $_inputArray[0])))
+				$_outputArray[$_arrayName] = $_element;	
 		}
 	}
 	return $_outputArray;
