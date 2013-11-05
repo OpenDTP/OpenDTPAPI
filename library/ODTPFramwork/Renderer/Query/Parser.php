a230efb0581f3aacaa796d7e70af786be19e2e11
Michael FORASTE <michael.foraste@gmail.com>
2013-11-05 01:42:29 +0100

    format string for an easier parsing

--- library/ODTPFramwork/Renderer/Query/Parser.php
+++ library/ODTPFramwork/Renderer/Query/Parser.php
@@ -8,8 +8,17 @@ class ODTPFramwork_Renderer_Query_Parser
 	*/
 	public function parseString($str)
 	{
+		if (!is_string($str)) {
+			throw new ODTPFramwork_Renderer_Query_Exception('$str must be a string');
+		}
 		$parsed_query = array();
+		$formated_query = preg_replace('#[\s]*([,=])[\s]*#', '$1', $str);
+		$parameters = explode(' ', $formated_query);
+		foreach ($parameters as $parameter) {
+			$parsed_query[] = $parameter;
+		}
 
+		echo '<pre>' . print_r($parsed_query, true) . '</pre>';die;
 		return $parsed_query;
 	}
 }
