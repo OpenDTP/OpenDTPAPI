0d25b3bfcce47ef964f24c3929420ebc9ae4d5ff
Michael FORASTE <michael.foraste@gmail.com>
2013-11-05 02:01:42 +0100

    ending parsing

--- library/ODTPFramwork/Renderer/Query/Parser.php
+++ library/ODTPFramwork/Renderer/Query/Parser.php
@@ -2,6 +2,10 @@
 
 class ODTPFramwork_Renderer_Query_Parser
 {
+	protected $_keywords = array(
+		'render', 'deconstruct', 'getinfo', 'input', 'output'
+	);
+
 	/**
 	* parses the provided command
 	* @return multidimensional array
@@ -14,11 +18,22 @@ class ODTPFramwork_Renderer_Query_Parser
 		$parsed_query = array();
 		$formated_query = preg_replace('#[\s]*([,=])[\s]*#', '$1', $str);
 		$parameters = explode(' ', $formated_query);
+		$current_keyword = null;
 		foreach ($parameters as $parameter) {
-			$parsed_query[] = $parameter;
+			$lower_parameter = strtolower($parameter);
+			if (!in_array($lower_parameter, $this->_keywords) && is_null($current_keyword)) {
+				throw new ODTPFramwork_Renderer_Query_Exception("Parse error in query : $str --- Near : $parameter");
+			} else if (in_array($lower_parameter, $this->_keywords)) {
+				$current_keyword = $lower_parameter;
+			} else {
+				if (isset($parsed_query[$current_keyword])) {
+					throw new ODTPFramwork_Renderer_Query_Exception("Parse error in query : $str --- Near : $parameter");
+				}
+				$parsed_query[$current_keyword] = $parameter;
+				$current_keyword = null;
+			}
 		}
 
-		echo '<pre>' . print_r($parsed_query, true) . '</pre>';die;
 		return $parsed_query;
 	}
 }
