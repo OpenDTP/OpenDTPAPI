2ff70e0e977b7ab212041a7104c23ad2493a8869
Michael FORASTE <m.foraste@gmail.com>
2014-05-07 02:05:51 +0200

    add configurations folder

---
+++ configurations/const.php
@@ -0,0 +1,11 @@
+<?php
+
+define('API_VERSION', '0.1 ALPHA');
+
+// Define path to application directory
+defined('APPLICATION_PATH')
+|| define('APPLICATION_PATH', realpath(dirname(__FILE__) . '/../application'));
+
+// Define application environment
+defined('APPLICATION_ENV')
+|| define('APPLICATION_ENV', (getenv('APPLICATION_ENV') ? getenv('APPLICATION_ENV') : 'production'));
