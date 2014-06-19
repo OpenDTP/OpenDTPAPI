c1a6ac3737e680f6fe8cad33778568be47320dab
Michael FORASTE <michael.foraste@gmail.com>
2014-06-19 14:09:50 +0200

    ref#179 starting a validation service

--- app/config/app.php
+++ app/config/app.php
@@ -114,6 +114,7 @@ return array(
         'Illuminate\View\ViewServiceProvider',
         'Illuminate\Workbench\WorkbenchServiceProvider',
         'Creolab\LaravelModules\ServiceProvider',
+        'App\Services\ValidationServiceProvider'
 
     ),
     /*
