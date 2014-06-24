665f5c76ce02b7d931a990f39e045feb863148f3
Michael FORASTE <michael.foraste@gmail.com>
2014-06-24 14:28:01 +0200

    dev/ref#181 Starting database seeding

---
+++ app/modules/auth/seeds/TokenSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Modules\Core\Seeds;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use App\Modules\Core\Models\User;
+
+class TokenSeeder extends Seeder
+{
+
+    public function run()
+    {
+        // Default OpenDTP app token
+    }
+}
