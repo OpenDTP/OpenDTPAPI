f0f0209e9c888d9a2fa4440eb1050961ce46a18a
Michael FORASTE <michael.foraste@gmail.com>
2014-08-12 08:51:57 +0200

    dev/ref#178 Adding storage module with facade

---
+++ app/modules/storage/Manager/StorageManager.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Modules\Storage\Manager;
+
+
+class StorageManager {
+    public function store($adapter) {
+        var_dump('debug');die;
+    }
+} 
---
+++ app/modules/storage/seeds/DatabaseSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Modules\Storage\Seeds;
+
+use Eloquent;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Eloquent::unguard();
+    }
+}
