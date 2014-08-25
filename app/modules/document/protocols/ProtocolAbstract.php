f0950c4b965a825d197f8007686d95a6b084c81d
Gabriel de Tassigny <gabriel@multivac.cc>
2014-08-25 15:46:05 +0200

    Fix some typo and get Indesign corresponding renderer entry

--- app/modules/document/protocols/ProtocolAbstract.php
+++ app/modules/document/protocols/ProtocolAbstract.php
@@ -2,11 +2,16 @@
 
 namespace App\Modules\Document\Protocols;
 
+use App\Modules\Document\Models\Renderer;
+use Illuminate\Support\Facades\Log;
+
 abstract class ProtocolAbstract implements IProtocol
 {
-  public function __construct()
+  protected $_renderer = null;
+
+  public function __construct(Renderer $renderer)
   {
-    
+    $this->_renderer = $renderer;
   }
 }
 
--- app/modules/document/protocols/indesign/Soap.php
+++ app/modules/document/protocols/indesign/Soap.php
@@ -3,10 +3,20 @@
 namespace App\Modules\Document\Protocols\Indesign;
 
 use App\Modules\Document\Protocols\ProtocolAbstract;
+use App\Modules\Document\Models\Renderer;
+use App\Modules\Document\Models\Connector;
+use Illuminate\Support\Facades\Log;
 
-class Soap extends ProcotolAbstract
+class Soap extends ProtocolAbstract
 {
-  public fonction connect()
+  public function __construct()
+  {
+    $connector = Connector::where('name', '=', 'indesign_soap')->first();
+    $renderer = Renderer::where('connector_id', '=', $connector->id)->first();
+    parent::__construct($renderer);
+  }
+
+  public function connect()
   {
 
   }
@@ -18,7 +28,7 @@ class Soap extends ProcotolAbstract
 
   public function disconnect()
   {
-    
+
   }
 }
 
