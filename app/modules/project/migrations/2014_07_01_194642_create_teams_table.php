c22b4ea09205c68478ffb6f27ac1307dfa629fcf
Gaetan Gueraud <gaetan.gueraud@gmail.com>
2014-09-02 08:03:50 +0200

    Redeploy fail

---
+++ app/modules/project/migrations/2014_07_01_194642_create_teams_table.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTeamsTable extends Migration
+{
+
+  /**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+			Schema::dropIfExists('teams');
+			Schema::create(
+					'teams',
+					function (Blueprint $table) {
+							$table->increments('id');
+							$table->string('name');
+							$table->text('description')
+									->nullable();
+							$table->foreign('user_id')
+									->references('id')
+									->on('users');
+							$table->timestamps();
+					}
+			);
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::table(
+				'teams',
+				function (Blueprint $table) {
+						$table->drop();
+				}
+		);
+	}
+
+}
